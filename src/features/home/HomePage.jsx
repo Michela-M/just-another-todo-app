56b63392a1ca36d903fbf7ec1f1464a89f51e08c
Michela-M <michela_mullins@outlook.com>
2025-12-02 19:51:15 +0100

    feat: update imports

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 import { useState, useEffect } from "react";
-import { saveTask } from "../../services/saveTask";
-import { getTasks } from "../../services/getTasks";
-import { toggleTaskCompletion } from "../../services/toggleTaskCompletion";
-import { deleteTask } from "../../services/deleteTask";
+import { saveTask } from "../../services/tasks/saveTask";
+import { getTasks } from "../../services/tasks/getTasks";
+import { toggleTaskCompletion } from "../../services/tasks/toggleTaskCompletion";
+import { deleteTask } from "../../services/tasks/deleteTask";
 import TaskSection from "./components/TaskSection";
 
 export default function HomePage() {
@@ -58,7 +58,7 @@ export default function HomePage() {
   return (
     <div className="bg-amber-50">
       <div className="max-w-[40%] mx-auto flex flex-col items-center gap-6 min-h-screen justify-center">
-        <h1 className="text-4xl text-primary">Today`&apos;`s Tasks</h1>
+        <h1 className="text-4xl text-primary">Today&apos;s Tasks</h1>
         <div className="flex gap-2 w-full">
           <Input
             placeholder="Enter a task"
